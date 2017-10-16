13ec9f919f6c4477076e0100d13f7095eafa182d
Cedric Michaux <cedric@he8us.be>
2017-10-16 16:24:40 +0200

    Update PHP config in all cases

--- 7.0/entrypoint.sh
+++ 7.0/entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-
-if [ "$1" = 'php-fpm' ]; then
-    export TIMEZONE=${TIMEZONE:-Europe/Brussels}
-
-    /usr/local/bin/confd -onetime -backend env
-fi
+export TIMEZONE=${TIMEZONE:-Europe/Brussels}
+/usr/local/bin/confd -onetime -backend env
 
 exec "$@"
